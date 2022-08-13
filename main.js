846093fb3f547654701fd885f1306e7e5891f449
EC2 Default User <ec2-user@ip-172-31-37-182.us-west-2.compute.internal>
2022-08-13 03:15:51 +0000

    ストップウォッチ基本動作確認済み

--- main.js
+++ main.js
@@ -1,50 +1,50 @@
 /*HTMLで表示させるストップウォッチや各種ボタンの変数を取得させる*/
-let Timer=document.getElementById("timer");
-let Start=document.getElementById("start");
-let Stop =document.getElementById("stop");
-let Reset=document.getElementById("reset");
+const Timer = document.getElementById("timer");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const resetButton = document.getElementById("reset");
 
 /*開始時刻*/
 let startTime;
 /*停止時刻*/
-let stopTime=0;
+let stopTime = 0;
 /*ストップウォッチ用変数*/
 let timeoutID;
 
 /*関数を用いて時間を表示させる*/
 function displayTime(){
- let currentTime=new Date(Date.now() - startTime + stopTime);
- let h =String(currentTime.getHours()-9).padStart(2,"0");
- let m =String(currentTime.getMinutes()).padStart(2,"0");
- let s =String(currentTime.getSeconds()).padStart(2,"0");
- let ms=String(currentTime.getMilliseconds()).padStart(3,"0");
+ const currentTime = new Date(Date.now() - startTime + stopTime);
+ const h = String(currentTime.getHours() - 9).padStart(1,"0");
+ const m = String(currentTime.getMinutes()).padStart(1,"0");
+ const s = String(currentTime.getSeconds()).padStart(1,"0");
+ const ms = String(currentTime.getMilliseconds()).padStart(1,"0");
 
- Timer.textContent=`${h}:${m}:${s}:${ms}`;
- timeoutID=setTimeout(displayTime(),10);}
+ Timer.textContent = `${h}:${m}:${s}:${ms}`;
+ timeoutID = setTimeout(displayTime,1);}
 
 /*startボタンの機能を追加する*/
-Start.addEventListener("click",() =>{
- Start.disabled=true;
- Stop.disabled=false;
- Reset.disabled=false;
- startTime=Date.now();
+startButton.addEventListener("click",() => {
+ startButton.disabled = true;
+ stopButton.disabled = false;
+ resetButton.disabled = false;
+ startTime = Date.now();
  displayTime();
 });
 
 /*stopボタンの機能を追加する*/
-Stop.addEventListener("click",function() {
- Start.disabled=false;
- Stop.disabled=true;
- Reset.disabled=false;
+stopButton.addEventListener("click",function() {
+ startButton.disabled = false;
+ stopButton.disabled = true;
+ resetButton.disabled = false;
  clearTimeout(timeoutID);
- stopTime +=(Date.now()-startTime);
+ stopTime += (Date.now() - startTime);
 });
 
 /*resetボタンの機能を追加する*/
-Reset.addEventListener("click",function(){
- Start.disabled=false;
- Stop.disabled=true;
- Reset.disabled=true;
- Timer.textContent="00:00:00:000";
- stopTime=0;
+resetButton.addEventListener("click",function(){
+ startButton.disabled =false;
+ stopButton.disabled = true;
+ resetButton.disabled = true;
+ Timer.textContent = "0:0:0:0";
+ stopTime = 0;
 });
